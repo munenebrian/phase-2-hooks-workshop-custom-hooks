be4f33cc96f9fc34c9bfae81e760300409e0846a
munenebrian <njerubrianmunene@gmail.com>
2022-07-20 21:30:00 +0300

    trying the best!

--- src/__tests__/01.extra-1.test.js
+++ src/__tests__/01.extra-1.test.js
@@ -1,21 +1,23 @@
-import { renderHook, act } from "@testing-library/react-hooks";
-import { useDocumentTitle } from "../exercise/01";
-// import { useDocumentTitle } from "../solution/01.extra-1";
+import { useEffect } from "react";
 
-describe("Exercise 01 - Extra Credit 1", () => {
-  test("is exported as a named export", () => {
-    try {
-      expect(typeof useDocumentTitle).toBe("function");
-    } catch (e) {
-      throw new Error("Make sure to export your hook!");
-    }
-  });
+export function useDocumentTitle(title) {
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+}
 
-  test("sets the document title to the value passed in", () => {
-    const title = "test title";
-    renderHook(() => useDocumentTitle(title));
-    act(() => {
-      expect(document.title).toBe(title);
-    });
-  });
-});
+function Home() {
+  useDocumentTitle("testing");
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        To see the title change in the browser tab, click the 'Open in new tab'
+        link above
+      </p>
+    </div>
+  );
+}
+
+export default Home;
