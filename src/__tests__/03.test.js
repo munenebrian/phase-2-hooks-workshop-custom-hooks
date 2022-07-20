38307c9a05ed9c0a606f5d66a508dafb4cd01708
munenebrian <njerubrianmunene@gmail.com>
2022-07-20 21:35:02 +0300

    trying the best!

--- src/__tests__/03.test.js
+++ src/__tests__/03.test.js
@@ -1,27 +1,35 @@
-import { renderHook } from "@testing-library/react-hooks/pure";
-import { fireEvent } from "@testing-library/react";
-import { useMouseCoordinates } from "../exercise/03";
-// import { useMouseCoordinates } from "../solution/03";
+import { useState, useEffect } from "react";
+import hasi from "../data/assets/hasi.png";
 
-describe("Exercise 03", () => {
-  test("returns an initial state with 0, 0 as the mouse coordinates", () => {
-    const { result } = renderHook(() => useMouseCoordinates());
-    expect(result.current).toMatchObject({ clientX: 0, clientY: 0 });
+export function useMouseCoordinates() {
+  const [coordinates, setCoordinates] = useState({
+    clientX: 0,
+    clientY: 0,
   });
 
-  test("returns the mouse coordinates after the mouse has moved", () => {
-    const { result } = renderHook(() => useMouseCoordinates());
-    fireEvent.mouseMove(window, {
-      clientX: 100,
-      clientY: 200,
-    });
-    expect(result.current).toMatchObject({ clientX: 100, clientY: 200 });
-  });
+  useEffect(() => {
+    function handleMouseMove({ clientX, clientY }) {
+      setCoordinates({ clientX, clientY });
+    }
 
-  test("the event handler function is removed when the component unmounts", () => {
-    const spy = jest.spyOn(window, "removeEventListener");
-    const { unmount } = renderHook(() => useMouseCoordinates());
-    unmount();
-    expect(spy).toHaveBeenCalledWith("mousemove", expect.any(Function));
-  });
-});
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return function cleanup() {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return coordinates;
+}
+
+export default function MyComponent() {
+  const { clientX, clientY } = useMouseCoordinates();
+
+  return (
+    <div style={{ cursor: "none", width: "100%", height: "100%" }}>
+      <h2>Mouse X: {clientX}</h2>
+      <h2>Mouse Y: {clientY}</h2>
+      <Cursor x={clientX} y={clientY} />
+    </div>
+  );
+}
